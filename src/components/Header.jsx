8da145d174e060987282b29340889b27bdec237b
easylearningbd <elebdsoft@gmail.com>
2024-01-15 03:56:31 +0600

    p153 Header Design Tailwind Css React Part 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdEmail } from "react-icons/md";
 import { IoMdPhonePortrait } from "react-icons/io";
-import { FaFacebookF, FaLock, FaUser } from "react-icons/fa";
+import { FaFacebookF, FaList, FaLock, FaUser } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
+    const [showShidebar, setShowShidebar] = useState(true);
     const user = true
 
     return (
@@ -57,18 +58,29 @@ const Header = () => {
                 <span>Login </span>
                  </Link>
         }
-
-
-
-
+ 
                             </div>
-                        </div>
+                        </div> 
+                    </div> 
+                </div> 
+            </div>
 
-                    </div>
 
+        <div className='w-white'>
+         <div className='w-[85%] lg:w-[90%] mx-auto'>
+            <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+                <Link to='/'>
+                    <img src="http://localhost:3000/images/logo.png" alt="" />
+                </Link>
+                <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
+                    <span> <FaList/> </span>
                 </div>
+            </div>
 
             </div>
+        </div>
+
+
            
         </div>
     );
