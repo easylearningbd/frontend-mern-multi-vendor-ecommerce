64f5f8d1fb6b677b4154f47553478929d7458f1f
easylearningbd <elebdsoft@gmail.com>
2024-01-15 04:29:29 +0600

    p154 Header Design Tailwind Css React Part 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,13 @@ import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
  
 
 const Header = () => {
-
+    const {pathname} = useLocation()
+     
     const [showShidebar, setShowShidebar] = useState(true);
     const user = true
 
@@ -69,14 +70,49 @@ const Header = () => {
         <div className='w-white'>
          <div className='w-[85%] lg:w-[90%] mx-auto'>
             <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+              
+                <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
+                    <div className='flex justify-between items-center'>
+ 
                 <Link to='/'>
                     <img src="http://localhost:3000/images/logo.png" alt="" />
                 </Link>
                 <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
                     <span> <FaList/> </span>
                 </div>
+                </div> 
+                </div>
+ 
+            <div className='md:lg:w-full w-9/12'>
+                <div className='flex justify-between md-lg:justify-center items-center flex-wrap pl-8'>
+                    <ul className='flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden'>
+                        <li>
+                            <Link className={`p-2 block ${pathname === '/' ?  'text-[#059473]' : 'text-slate-600' } `} >Home</Link>
+                        </li>
+
+                        <li>
+                            <Link className={`p-2 block ${pathname === '/shop' ?  'text-[#059473]' : 'text-slate-600' } `} >Shop</Link>
+                        </li>
+                        <li>
+                            <Link className={`p-2 block ${pathname === '/blog' ?  'text-[#059473]' : 'text-slate-600' } `} >Blog</Link>
+                        </li>
+                        <li>
+                            <Link className={`p-2 block ${pathname === '/about' ?  'text-[#059473]' : 'text-slate-600' } `} >About Us</Link>
+                        </li>
+                        <li>
+                            <Link className={`p-2 block ${pathname === '/contact' ?  'text-[#059473]' : 'text-slate-600' } `} >Contact Us</Link>
+                        </li>
+
+                    </ul>
+
+                </div>
+
             </div>
 
+
+
+
+            </div> 
             </div>
         </div>
 
