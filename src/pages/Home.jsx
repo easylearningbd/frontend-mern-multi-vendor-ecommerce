2489c2da6326533293866a389108288178066642
easylearningbd <elebdsoft@gmail.com>
2024-01-16 04:15:58 +0600

    p162 Banner Design Uailwind Css React Part 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import Categorys from '../components/Categorys';
 
 const Home = () => {
     return (
         <div className='w-full'>
             <Header/>
             <Banner/>
+            <Categorys/>
         </div>
     );
 };
