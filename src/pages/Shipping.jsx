354f2bdc39e5f0d6ccef944af537f12b98d64e9b
easylearningbd <elebdsoft@gmail.com>
2024-01-19 05:53:13 +0600

    p186 Shipping Page Design Setup Part 4

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -124,7 +124,7 @@ const Shipping = () => {
                 <h2 className='text-slate-600 font-semibold pb-2'>Deliver To {state.name}</h2>
                 <p>
                     <span className='bg-blue-200 text-blue-800 text-sm font-medium mr-2 px-2 py-1 rounded'>Home</span>
-                    <span>address , provice  ctyu </span>
+                    <span>{state.phone} {state.address} {state.province} {state.city} {state.area}  </span>
 
                     <span onClick={() => setRes(false)} className='text-indigo-500 cursor-pointer'>Change </span>
                 </p>
@@ -133,15 +133,90 @@ const Shipping = () => {
 
             </div>
             }
+              </div>
 
-            
+              {
+                   [1,2].map((p,i) => <div className='flex bg-white p-4 flex-col gap-2'>
+                   <div className='flex justify-start items-center'>
+                       <h2 className='text-md text-slate-600 font-bold'>Easy Shop</h2>
+                   </div>
+
+                   {
+                       [1,2].map((p,i) => <div className='w-full flex flex-wrap'>
+                       <div className='flex sm:w-full gap-2 w-7/12'>
+                           <div className='flex gap-2 justify-start items-center'>
+                       <img className='w-[80px] h-[80px]' src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
+                       <div className='pr-4 text-slate-600'>
+                       <h2 className='text-md font-semibold'>Product Name </h2>
+                       <span className='text-sm'>Brand: Jara</span>
+                       </div>
+                           </div>
+                       </div>
+
+   <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
+       <div className='pl-4 sm:pl-0'>
+           <h2 className='text-lg text-orange-500'>$240</h2>
+           <p className='line-through'>$300</p>
+           <p>-15%</p>
+       </div>
+       <div className='flex gap-2 flex-col'>
+           <div className='flex bg-slate-200 h-[30px] justify-center items-center text-xl'>
+               <div className='px-3 cursor-pointer'>-</div> 
+               <div className='px-3'>2</div> 
+               <div className='px-3 cursor-pointer'>+</div> 
+           </div>
+           <button className='px-5 py-[3px] bg-red-500 text-white'>Delete</button>
+       </div>
+   </div>
 
 
+                   </div>)
+                   }
 
-                    </div>
+               </div>) 
+                } 
+ 
+
+                </div> 
+            </div>
+
+            <div className='w-[33%] md-lg:w-full'>
+    <div className='pl-3 md-lg:pl-0 md-lg:mt-5'>
+        
+            <div className='bg-white p-3 text-slate-600 flex flex-col gap-3'>
+                <h2 className='text-xl font-bold'>Order Summary</h2>
+                <div className='flex justify-between items-center'>
+                    <span>Items Total (5) </span>
+                    <span>$343 </span>
+                </div>
+                <div className='flex justify-between items-center'>
+                    <span>Delivery Fee </span>
+                    <span>$40 </span>
                 </div>
 
+                <div className='flex justify-between items-center'>
+                    <span>Total Payment </span>
+                    <span>$450 </span>
+                </div>
+               
+
+                <div className='flex justify-between items-center'>
+                    <span>Total</span>
+                    <span className='text-lg text-[#059473]'>$490 </span>
+                </div>
+                <button disabled={res ? false : true} className={`px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg ${res ? 'bg-red-500' : 'bg-red-300'}  text-sm text-white uppercase`}>
+                   Place Order 
+                </button>
+
             </div>
+        
+
+    </div>
+
+</div>
+
+
+
             </div>  
 
         </div>
