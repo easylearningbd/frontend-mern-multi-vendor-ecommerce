c602a72c9ec81e337e28b180347e6b2bde07e58e
easylearningbd <elebdsoft@gmail.com>
2024-01-20 05:18:05 +0600

    p193 Product Destails Page Design Part 7

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -228,14 +228,48 @@ const Details = () => {
     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
         }
+    </div> 
+         </div> 
+         </div>
+
+<div className='w-[28%] md-lg:w-full'>
+<div className='pl-4 md-lg:pl-0'>
+    <div className='px-3 py-2 text-slate-600 bg-slate-200'>
+        <h2 className='font-bold'>From Easy Shop</h2>
     </div>
-
-
-
-
-                </div>
-
+    <div className='flex flex-col gap-5 mt-3 border p-3'>
+        {
+            [1,2,3].map((p,i) => {
+                return (
+        <Link className='block'>
+            <div className='relative h-[270px]'>
+            <img className='w-full h-full' src={`http://localhost:3000/images/products/${p}.webp`} alt="" /> 
+            {
+            discount !== 0 && <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{discount}%
             </div>
+            }
+            </div>
+
+            <h2 className='text-slate-600 py-1 font-bold'>Product Name </h2>
+            <div className='flex gap-2'>
+                <h2 className='text-lg font-bold text-slate-600'>$434</h2>
+                <div className='flex items-center gap-2'>
+                    <Rating ratings={4.5}  />
+                </div>
+            </div>
+            
+        </Link>
+                )
+            })
+        }
+
+    </div>
+</div>
+</div>
+
+
+
+
             </div> 
         
         
