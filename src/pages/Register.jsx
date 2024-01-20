c3a0b8ae18ead095ef0c930ad98a369deb625822
easylearningbd <elebdsoft@gmail.com>
2024-01-21 04:06:04 +0600

    p201 Customer Login Register  Page Design Part 2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { FaFacebookF } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa6"; 
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
@@ -10,8 +13,7 @@ const Register = () => {
         <div className='w-full justify-center items-center p-10'>
             <div className='grid grid-cols-2 w-[60%] mx-auto bg-white rounded-md'>
                 <div className='px-8 py-8'>
-            <h2 className='text-center w-full text-xl text-slate-600 font-bold'>Register </h2>
-                </div>
+            <h2 className='text-center w-full text-xl text-slate-600 font-bold'>Register </h2> 
 
     <div>
         <form className='text-slate-600'>
@@ -32,14 +34,35 @@ const Register = () => {
     </div>
 
     <button className='px-8 w-full py-2 bg-[#059473] shadow-lg hover:shadow-green-500/40 text-white rounded-md'>Register</button>
-
-
-
-
+ 
         </form>
+    <div className='flex justify-center items-center py-2'>
+        <div className='h-[1px] bg-slate-300 w-[95%]'> </div>
+        <span className='px-3 text-slate-600'>Or</span>
+        <div className='h-[1px] bg-slate-300 w-[95%]'> </div>
+    </div>
+
+    <button className='px-8 w-full py-2 bg-indigo-500 shadow hover:shadow-indigo-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
+        <span><FaFacebookF /> </span>
+        <span>Login With Facebook </span>
+    </button>
+
+    <button className='px-8 w-full py-2 bg-red-500 shadow hover:shadow-red-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
+        <span><FaGoogle  /></span>
+        <span>Login With Google </span>
+    </button> 
     </div>    
 
+    <div className='text-center text-slate-600 pt-1'>
+        <p>You Have No Account? <Link className='text-blue-500' to='/login'> Login</Link> </p>
+    </div> 
             </div> 
+
+        <div className='w-full h-full py-4 pr-4'>
+            <img src="http://localhost:3000/images/login.jpg" alt="" />
+         </div>    
+
+         </div>
         </div>
     </div>        
             
