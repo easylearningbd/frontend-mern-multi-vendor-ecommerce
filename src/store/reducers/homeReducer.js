e51970d11a115b485009c6b8311908222d95740a
easylearningbd <elebdsoft@gmail.com>
2024-01-29 01:46:17 +0600

    p213 Fetch Price Range and Category and Show Part 3

--- src/store/reducers/homeReducer.js
+++ src/store/reducers/homeReducer.js
@@ -55,6 +55,10 @@ export const homeReducer = createSlice({
         latest_product : [],
         topRated_product : [],
         discount_product : [],
+        priceRange : {
+            low: 0,
+            high: 100
+        }
     },
     reducers : {
  
@@ -69,6 +73,10 @@ export const homeReducer = createSlice({
             state.latest_product = payload.latest_product;
             state.topRated_product = payload.topRated_product;
             state.discount_product = payload.discount_product;
+        })
+        .addCase(price_range_product.fulfilled, (state, { payload }) => { 
+            state.latest_product = payload.latest_product;
+            state.priceRange = payload.priceRange; 
         })
 
     }
