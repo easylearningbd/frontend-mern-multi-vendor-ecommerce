3fffdc51d501c5515e064a6a16b831c4a7b22ab0
easylearningbd <elebdsoft@gmail.com>
2024-02-08 18:23:29 +0600

    p271 Customer Dashboard Data Get Part 2

--- src/store/reducers/dashboardReducer.js
+++ src/store/reducers/dashboardReducer.js
@@ -5,7 +5,7 @@ export const get_dashboard_index_data = createAsyncThunk(
     'dashboard/get_dashboard_index_data',
     async(userId, { rejectWithValue,fulfillWithValue }) => {
         try {
-            const {data} = await api.get(`/home/coustomer/get-dashboard-date/${userId}`) 
+            const {data} = await api.get(`/home/coustomer/get-dashboard-data/${userId}`) 
             console.log(data)
             // return fulfillWithValue(data)
         } catch (error) {
