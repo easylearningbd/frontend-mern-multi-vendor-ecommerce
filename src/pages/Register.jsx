ce9b1c9effdb94e965dadc7886313196ced706fa
easylearningbd <elebdsoft@gmail.com>
2024-02-27 18:54:44 +0600

    p396 Mobile Responsive Dashboard and Web Side Connect Part 1

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -107,6 +107,20 @@ const Register = () => {
     <div className='text-center text-slate-600 pt-1'>
         <p>You Have No Account? <Link className='text-blue-500' to='/login'> Login</Link> </p>
     </div> 
+
+    <a target='_blank' href="http://localhost:3001/login">
+     <div className='px-8 w-full py-2 bg-[#02e3e0] shadow hover:shadow-red-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
+            Login As a Seller
+     </div>
+     </a>
+
+     <a target='_blank' href="http://localhost:3001/register">
+     <div className='px-8 w-full py-2 bg-[#ad2cc4] shadow hover:shadow-red-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
+            Register As a Seller
+     </div>
+     </a>
+
+
             </div> 
 
         <div className='w-full h-full py-4 pr-4'>
