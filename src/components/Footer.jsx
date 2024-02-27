63e9f386ae0d96d487b00aacecfdff94f879f25e
easylearningbd <elebdsoft@gmail.com>
2024-02-27 18:40:47 +0600

    p395 Error Bugs Fix Part 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaFacebookF} from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { useSelector } from 'react-redux';
+import { FaHeart } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Footer = () => {
+
+    const navigate = useNavigate() 
+    const {userInfo} = useSelector(state => state.auth) 
+    const {card_product_count,wishlist_count} = useSelector(state => state.card) 
+
     return (
         <footer className='bg-[#f3f6fa]'>
             <div className='w-[85%] flex flex-wrap mx-auto border-b py-16 md-lg:pb-10 sm:pb-6'>
@@ -98,6 +106,41 @@ const Footer = () => {
             <div className='w-[90%] flex flex-wrap justify-center items-center text-slate-600 mx-auto py-5 text-center'>
                 <span>Copiright @ 2024 All Rights Reserved </span>
             </div>
+
+
+    <div className='hidden fixed md-lg:block w-[50px] h-[110px] bottom-3 right-2 bg-white rounded-full p-2'>
+        <div className='w-full h-full flex gap-3 flex-col justify-center items-center'>
+        <div onClick={() => navigate(userInfo ? '/card' : '/login') }  className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+            <span className='text-xl text-green-500'><FaCartShopping/></span>
+            {
+                card_product_count !== 0 && <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]'>
+                    {
+                        card_product_count
+                    }
+                </div>
+            }
+            
+            
+        </div>
+
+        <div  onClick={() => navigate(userInfo ? '/dashboard/my-wishlist' : '/login') } className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+            <span className='text-xl text-green-500'><FaHeart/></span>
+            {
+                wishlist_count !== 0 && <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]'>
+                    {
+                       wishlist_count 
+                    }
+                </div>
+            }
+            
+        </div>
+
+        </div>
+    </div>
+
+
+
+
            
         </footer>
     );
